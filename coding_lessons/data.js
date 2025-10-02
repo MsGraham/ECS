22c3bb95a782616fc08320cf1ccb03f1fb79d85c
MsGraham <kgraham@jmchs.org>
2025-10-02 17:27:45 -0600

    update loops quiz questions

---
+++ coding_lessons/data.js
@@ -0,0 +1,17 @@
+
+
+quiz = [
+  { 
+    q: "what color is the sky?",
+    a: "blue"
+  },
+  {
+    q: "is a keyboard input or output?",
+    a: "input"
+  },
+  {
+    q: "is a mechanical sewing machine digital or analog?",
+    a: "analog"
+  }
+
+]
